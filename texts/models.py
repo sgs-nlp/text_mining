de567c9f41425b28c17c23fdce7662b0225fe84b
Navid <n.soleymani@pnr.ir>
2021-05-19 13:33:14 +0430

    create text tables

--- texts/models.py
+++ texts/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+
+class Corpus(models.Model):
+    documents = models.ManyToManyField(
+        to='Document',
+    )
+    stopwords = models.ManyToManyField(
+        to='StopWord',
+    )
+    keywords = models.ManyToManyField(
+        to='KeyWord',
+    )
+    topics = models.ManyToManyField(
+        to='Topic',
+    )
+
+
+class Document(models.Model):
+    paragraphs = models.ManyToManyField(
+        to='Paragraph',
+    )
+    stopwords = models.ManyToManyField(
+        to='StopWord',
+    )
+    keywords = models.ManyToManyField(
+        to='KeyWord',
+    )
+    topics = models.ManyToManyField(
+        to='Topic',
+    )
+
+
+class Paragraph(models.Model):
+    sentences = models.ManyToManyField(
+        to='Sentence',
+    )
+    stopwords = models.ManyToManyField(
+        to='StopWord',
+    )
+    keywords = models.ManyToManyField(
+        to='KeyWord',
+    )
+    topics = models.ManyToManyField(
+        to='Topic',
+    )
+
+
+class Sentence(models.Model):
+    words = models.ManyToManyField(
+        to='Word',
+    )
+    stopwords = models.ManyToManyField(
+        to='StopWord',
+    )
+    keywords = models.ManyToManyField(
+        to='KeyWord',
+    )
+    topics = models.ManyToManyField(
+        to='Topic',
+    )
+
+
+class KeyWord(models.Model):
+    word = models.ForeignKey(
+        to='Word',
+        on_delete=models.CASCADE,
+    )
+
+
+class StopWord(models.Model):
+    word = models.ForeignKey(
+        to='Word',
+        on_delete=models.CASCADE,
+    )
+
+
+class Topic(models.Model):
+    word = models.ForeignKey(
+        to='Word',
+        on_delete=models.CASCADE,
+    )
+
+
+class Word(models.Model):
+    char = models.CharField(
+        max_length=64,
+        unique=True,
+        null=False,
+        blank=False,
+    )
