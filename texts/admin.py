74a7ae2c232515fe896942584f77d1ac3f548cd6
Navid <n.soleymani@pnr.ir>
2021-05-19 13:34:03 +0430

    create django admin panel for texts tables

--- texts/admin.py
+++ texts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Corpus)
+class CorpusModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Document)
+class DocumentModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Paragraph)
+class ParagraphModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Sentence)
+class SentenceModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(KeyWord)
+class KeyWordModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(StopWord)
+class StopWordModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Word)
+class WordModelAdmin(admin.ModelAdmin):
+    pass
