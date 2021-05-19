0a50600b8f5964b3c465cbaf78c60a478d333640
Navid <n.soleymani@pnr.ir>
2021-05-19 13:30:40 +0430

    add texts application

--- text_mining/settings.py
+++ text_mining/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'texts.apps.TextsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ texts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+app_name = 'texts'
+
+urlpatterns = [
+    path('api', include('texts.api.urls')),
+]
