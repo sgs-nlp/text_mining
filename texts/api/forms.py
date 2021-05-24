c4460f87b858a44bdef24ebb3a25ae2ce8da9af3
Navid <n.soleymani@pnr.ir>
2021-05-24 13:45:29 +0430

    add output fields for document

--- texts/api/forms.py
+++ texts/api/forms.py
@@ -3,11 +3,32 @@ import json
 
 
 class DocumentInput(forms.Form):
+    document = forms.CharField(
+        required=False,
+        max_length=2048,
+    )
     document_path = forms.CharField(
+        required=False,
         max_length=512,
-        required=True,
     )
 
-    output_fields = forms.JSONField(
-        required=True,
+
+class DocumentOutput(forms.Form):
+    normal_document_path = forms.BooleanField(
+        required=False,
+    )
+    sentence_tokenize = forms.BooleanField(
+        required=False,
+    )
+    word_tokenize = forms.BooleanField(
+        required=False,
+    )
+    stop_words_list = forms.BooleanField(
+        required=False,
+    )
+    key_words_list = forms.BooleanField(
+        required=False,
+    )
+    save_to_file = forms.BooleanField(
+        required=False,
     )
