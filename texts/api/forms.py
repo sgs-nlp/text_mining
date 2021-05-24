e83d1d303c9a6c29f6f766175c8e993d65e131e2
Navid <n.soleymani@pnr.ir>
2021-05-24 16:35:50 +0430

    create forms for clean data

--- texts/api/forms.py
+++ texts/api/forms.py
@@ -1,44 +1,36 @@
 from django import forms
-import json
 
 
 class DocumentInput(forms.Form):
-    document = forms.CharField(
-        required=False,
-        max_length=2048,
-    )
     document_path = forms.CharField(
         required=False,
         max_length=512,
+    )
+    document = forms.CharField(
+        required=False,
+        max_length=2048,
     )
 
 
 class DocumentOutput(forms.Form):
     normal_document_path = forms.BooleanField(
         required=False,
-        initial=True,
-    )
-    sentence_tokenize = forms.BooleanField(
-        required=False,
-        initial=True,
-    )
-    word_tokenize = forms.BooleanField(
-        required=False,
-        initial=True,
-    )
-    stop_words_list = forms.BooleanField(
-        required=False,
-        initial=True,
-    )
-    key_words_list = forms.BooleanField(
-        required=False,
-        initial=True,
     )
     normal_document = forms.BooleanField(
         required=False,
-        initial=True,
+    )
+    sentence_tokenize = forms.BooleanField(
+        required=False,
+    )
+    word_tokenize = forms.BooleanField(
+        required=False,
+    )
+    stop_words_list = forms.BooleanField(
+        required=False,
+    )
+    key_words_list = forms.BooleanField(
+        required=False,
     )
     save_to_file = forms.BooleanField(
         required=False,
-        initial=True,
     )
