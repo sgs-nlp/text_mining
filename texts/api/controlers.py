fdd4d8fba96265e42d34b4b147a415fadf4a65ff
Navid <n.soleymani@pnr.ir>
2021-05-24 16:38:28 +0430

    create controller for document analysis

--- texts/api/controlers.py
+++ texts/api/controlers.py
@@ -1,7 +1,10 @@
+import json
+import os.path
 from typing import Optional
 from repository.pre_processing import edit_symbol_spacing
 from repository.extractor import Stopwords, Keywords
 from texts.models import Word
+
 
 class Document:
     def __init__(self, string):
@@ -11,6 +14,13 @@ class Document:
         self._words = None
         self._stopwords = None
         self._keywords = None
+        # todo
+        self._id = 1
+        self.add_to_db_flag = True
+
+    @property
+    def id(self):
+        return self.add_to_db()
 
     @property
     def is_valid(self) -> bool:
@@ -19,6 +29,8 @@ class Document:
     def add_to_db(self):
         if not self.is_valid:
             return None
+        if self.add_to_db_flag:
+            return self._id
         # word to db
         _d_words = self.words
         for _s_words in _d_words:
@@ -27,6 +39,9 @@ class Document:
                     wrd = Word()
                     wrd.char = _word
                     wrd.save()
+        # todo true id
+        self._id = 1
+        return self._id
 
     @property
     def keywords(self) -> Optional[list]:
@@ -98,6 +113,7 @@ class Document:
         self._document = h_normalizer.normalize(string)
         return self._document
 
+
 # class Sentence:
 #     def __init__(self, string):
 #         self.string = string
@@ -154,3 +170,64 @@ class Document:
 #         h_normalizer = Normalizer()
 #         self._word = h_normalizer.normalize(self.string)
 #         return self._word
+
+def document_analysis(input_fields, output_fields):
+    print(output_fields)
+    string = ''
+    document_path = None
+    if len(input_fields['document_path']):
+        document_path = input_fields['document_path']
+        with open(document_path, 'r', encoding='utf-8') as file:
+            if file is None:
+                raise Exception('file is not exist')
+            string = file.read()
+    elif len(input_fields['document']):
+        string = input_fields['document']
+
+    d = Document(string)
+    if not d.is_valid:
+        raise Exception('data is not valid')
+
+    if document_path is not None:
+        dir_name = os.path.dirname(document_path)
+        file_name = os.path.basename(document_path)
+        file_name_split = os.path.splitext(file_name)
+        name = file_name_split[0]
+        ext = file_name_split[1]
+    else:
+        from text_mining.settings import BASE_DIR
+        from datetime import datetime
+        dir_name = os.path.join(BASE_DIR, 'files')
+        name = str(hash(datetime.now()))
+        ext = '.file'
+    output = {'id': d.id}
+    if output_fields['normal_document_path'] is True:
+        normal_document_path = os.path.join(dir_name, f'{name}.normal{ext}')
+        with open(normal_document_path, 'w', encoding='utf-8') as file:
+            file.write(d.document)
+        output['normal_document_path'] = normal_document_path
+
+    if output_fields['normal_document'] is True:
+        output['normal_document'] = d.document
+
+    if output_fields['sentence_tokenize'] is True:
+        output['sentence_tokenize'] = d.sentences
+
+    if output_fields['word_tokenize'] is True:
+        output['word_tokenize'] = d.words
+
+    if output_fields['stop_words_list'] is True:
+        output['stop_words_list'] = d.stopwords
+
+    if output_fields['key_words_list'] is True:
+        output['key_words_list'] = d.keywords
+
+    if output_fields['save_to_file'] is True:
+        output_path = os.path.join(dir_name, f'{name}.output.json')
+        j_output = json.dumps(output)
+        print(j_output)
+        with open(output_path, 'w') as file:
+            file.write(j_output)
+        output['result_file_path'] = str(output_path)
+
+    return output
