843fd459eb0e1a6a9a7edc0ca5d3388bed90bf70
Navid <n.soleymani@pnr.ir>
2021-05-31 17:49:13 +0430

    user scoring

--- text_strings/api/routes.py
+++ text_strings/api/routes.py
@@ -4,9 +4,18 @@ from os.path import join
 from django.views.decorators.http import require_POST
 from django.http import HttpRequest
 
-from text_strings.api.forms import *
 from repository.decorators import response, response2
-from .controlers import word2db, sentence2db, document2db, keywords_extract, document_keywords, is_keywords
+from repository.converter import to_pydict
+from .forms import *
+from .controlers import (
+    word2db,
+    sentence2db,
+    document2db,
+    keywords_extract,
+    document_keywords,
+    is_keywords,
+    keyword2db,
+)
 
 
 @require_POST
@@ -21,18 +30,21 @@ def sentences2db_view(input_fields):
     return sentence2db(input_fields['sentence'])
 
 
+# todo
 @require_POST
 @response(Index2DBInput, Index2DBOutput)
 def index2db_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(IndexInput, IndexOutput)
 def index_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(IsIndexInput, IsIndexOutput)
 def is_index_view(input_fields, output_fields):
@@ -41,14 +53,14 @@ def is_index_view(input_fields, output_fields):
 
 @require_POST
 @response2(Documents2DBInput, Documents2DBOutput)
-def documents2db_view(input_fields):
+def document2db_view(input_fields):
     return document2db(input_fields['document'])
 
 
 @require_POST
-@response(Keywords2DBInput, Keywords2DBOutput)
-def keywords2db_view(input_fields, output_fields):
-    pass
+@response2(Keywords2DBInput)
+def keyword2db_view(input_fields):
+    return keyword2db(input_fields['document_id'], input_fields['word'])
 
 
 @require_POST
@@ -62,41 +74,50 @@ def keywords_view(input_fields):
 @require_POST
 @response2(IsKeywordsInput)
 def is_keywords_view(input_fields):
-    if len(input_fields['document_public_key']):
-        pass
-    return is_keywords(input_fields['document_public_key'], input_fields['keywords_score_list'])
+    keywords_score_list = input_fields['keywords_score_list']
+    keywords_score_list = to_pydict(keywords_score_list)
+    return is_keywords(
+        user_id=input_fields['user_id'],
+        keywords_score_list=keywords_score_list
+    )
 
 
+# todo
 @require_POST
 @response(Stopwords2DBInput, Stopwords2DBOutput)
 def stopwords2db_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(StopwordsInput, StopwordsOutput)
 def stopwords_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(IsStopwordsInput, IsStopwordsOutput)
 def is_stopwords_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(NormalizeInput, NormalizeOutput)
 def normalize_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(TokenizeInput, TokenizeOutput)
 def tokenize_view(input_fields, output_fields):
     pass
 
 
+# todo
 @require_POST
 @response(DocumentInput, DocumentOutput)
 def document_view(input_fields, output_fields):
